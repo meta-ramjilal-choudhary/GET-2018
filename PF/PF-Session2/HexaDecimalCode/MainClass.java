6fadc2e9e5d266aac29edd8fa79c5dc0778765c9
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-19 10:30:35 +0530

    Update MainClass.java

--- PF/PF-Session2/HexaDecimalCode/MainClass.java
+++ PF/PF-Session2/HexaDecimalCode/MainClass.java
@@ -11,43 +11,50 @@ public class MainClass {
 		
 		HexCalc hex = new HexCalc();
 		
-		System.out.print("Hexadecimal Numbers Calculation \n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n");
-		System.out.println(" 5. Compare\n");
-		System.out.println("Enter a Corrosponding Calculation id: ");
-		int id = sc.nextInt();
-		String result;
-		switch(id) {
-			case 1 : result = hex.add(number1, number2);
-					 System.out.println("Addition : " + result);
-					 break;
-			case 2 : result = hex.subtract(number1, number2);
-			  		 System.out.println("Subtraction : " + result);
-			  		 break;
-			case 3 : result = hex.multiply(number1, number2);
-			  		 System.out.println("Multiplication : " + result);
-			  		 break;
-			case 4 : try {
-				        result = hex.divide(number1, number2);
-	  		 			System.out.println("Division : " + result);
-	  		 		 }
-	  		 		 catch(ArithmeticException e) {
-	  		 			 System.out.println("Division by ZERO is not Possible !");
-	  		 		 }
-	  		 		 break;
-			case 5 : int comp = hex.compare(number1, number2);
-	  				 if(comp > 0) {
-						 System.out.println("number1 > number2");
-					 }
-					 else if(comp < 0) {
-						 System.out.println("number1 < number2");
-					 }
-					 else {
-						 System.out.println("number1 == number2");
-					 }
-	  				 break;		 
-			default : System.out.println("Invalid Input!");
-					  break;
+		if( !hex.validHex(number1)) {
+			System.out.println(number1 + " is not a valid hexadecimal number");
 		}
-		
+		else if (!hex.validHex(number2)) {
+			System.out.println(number2 + " is not a valid hexadecimal number");
+		}
+		else {
+			System.out.print("Hexadecimal Numbers Calculation \n 1. Add\n 2. Subtract\n 3. Multiply\n 4. Divide\n");
+			System.out.println(" 5. Compare\n");
+			System.out.println("Enter a Corrosponding Calculation id: ");
+			int id = sc.nextInt();
+			String result;
+			switch(id) {
+				case 1 : result = hex.add(number1, number2);
+						 System.out.println("Addition : " + result);
+						 break;
+				case 2 : result = hex.subtract(number1, number2);
+			  		 	 System.out.println("Subtraction : " + result);
+			  		 	 break;
+				case 3 : result = hex.multiply(number1, number2);
+			  		 	 System.out.println("Multiplication : " + result);
+			  		 	 break;
+				case 4 : try {
+				       	 	 result = hex.divide(number1, number2);
+				       	 	 System.out.println("Division : " + result);
+	  		 		 	 }
+	  		 		 	 catch(ArithmeticException e) {
+	  		 		 		 System.out.println("Division by ZERO is not Possible !");
+	  		 		 	 }
+	  		 		 	 break;
+				case 5 : int comp = hex.compare(number1, number2);
+					 	 if(comp > 0) {
+					 		 System.out.println("number1 > number2");
+					     }
+					 	 else if(comp < 0) {
+					 		 System.out.println("number1 < number2");
+					 	 }
+					 	 else {
+					 		 System.out.println("number1 == number2");
+					 	 }
+					 	 break;		 
+				default : System.out.println("Invalid Input!");
+					     break;
+			}
+		}
 	}
 }
