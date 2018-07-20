9b17b37bc1875c7e02f3c55b829439cc9222807c
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-20 15:11:37 +0530

    Update MainClassMarksheet.java

--- SCF/SCF-Session3/Quetion2/MainClassMarksheet.java
+++ SCF/SCF-Session3/Quetion2/MainClassMarksheet.java
@@ -4,9 +4,9 @@ public class MainClassMarksheet extends Marksheet {
 	public static void main(String args[]) {
 		double grades[] = {50.454, 44.6945, 60.6714, 50.3312, 40.2125, 10.2008};
 		Marksheet marksheet = new Marksheet(grades);
-		System.out.println(marksheet.averageOfAllGrades());
-		System.out.println(marksheet.maximumOfAllGrades());
-		System.out.println(marksheet.minimumOfAllGrades());
-		System.out.println(marksheet.percentageOfStudentPassed());
+		System.out.println("Average of All grades : " + marksheet.averageOfAllGrades());
+		System.out.println("Maximum of all grades: " + marksheet.maximumOfAllGrades());
+		System.out.println("Minimum of all grades: " + marksheet.minimumOfAllGrades());
+		System.out.println("Percentage of student passed: " + marksheet.percentageOfStudentPassed());
 	}
 }
