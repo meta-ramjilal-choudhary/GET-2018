35abd70b185af4742d04a7af5c5c57f0363c8c27
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-23 17:23:31 +0530

    Assignment 2 - Extended Shopping cart updated

--- PF/PF-Assignment2/FixedProductPromotion.java
+++ PF/PF-Assignment2/FixedProductPromotion.java
@@ -5,21 +5,18 @@ import java.text.ParseException;
 
 public class FixedProductPromotion extends ShoppingCart implements Promotion {
 	
-	private HashMap<Long, Double> fixedDiscount;
-	
 	FixedProductPromotion(){
-		fixedDiscount = new HashMap<>();
 	}
 	
 	/*
 	 * @param   item id (long), promo code (string)
 	 * @return  minimum item price
 	 */
-	public double getMinimumPrice(long itemID, String code) {
+	public double getMinimumPrice(int itemID, String code) {
 		
 		this.setMinimumPrice(itemID, code);
-		
-		return this.getPrice(itemID);		
+		ItemList item = this.searchItemInCart(itemID);
+		return item.getItemTotalPrice();
 	}
 	
 	/*
@@ -27,21 +24,24 @@ public class FixedProductPromotion extends ShoppingCart implements Promotion {
 	 * @return nothing
 	 * set minimum price for item after apply promo code discount and fixed discount
 	 */
-	public void setMinimumPrice(long itemID, String code) {
+	public void setMinimumPrice(int itemID, String code) {
+		
+		ItemList item = this.searchItemInCart(itemID);
+		double price = item.getItemTotalPrice();
+		System.out.println(price);
 		double priceAfterFixedDiscount ;
-		double price = this.getPrice(itemID);
+		priceAfterFixedDiscount = price - (price * (this.getItemFixedDiscount(itemID)/100.0));
 		
-		priceAfterFixedDiscount = price - (price * (this.fixedDiscount.get(itemID)/100.0));
-		
-		if (this.isPromotionApplicable( code)) {
+		if (this.isPromotionApplicable(code)) {
 			
 			double priceAfterPromoDiscount ;
 			priceAfterPromoDiscount = price - (price *(this.getPromoDiscount(code)/100.0));
+			
 			if (priceAfterFixedDiscount < priceAfterPromoDiscount) {
-				this.setPrice(itemID, priceAfterFixedDiscount);
+				item.setItemTotalPrice(priceAfterFixedDiscount);;
 			}
 			else {
-				this.setPrice(itemID, priceAfterPromoDiscount);
+				item.setItemTotalPrice(priceAfterPromoDiscount);
 			}
 		}
 	}
@@ -51,40 +51,23 @@ public class FixedProductPromotion extends ShoppingCart implements Promotion {
 	 * @return  discount on given promotion code
 	 */
 	public double getPromoDiscount(String code) {
-		
-		final double noDiscount = 0.0;
+
 		HashMap<String, Double> discount = new HashMap<>();
 		discount.put("sky10", 15.00);
-		discount.put("algo10", 10.00);
 		discount.put("laptop10", 25.00);
-		discount.put("samsung10", 40.00);
+		discount.put("mobile10", 40.00);
 	
 		return (discount.containsKey(code) ? discount.get(code) : 0.0);
-		
-// 		if(discount.containsKey(code)) {
-// 			return discount.get(code);
-// 		}
-// 		else {
-// 			return noDiscount;
-// 		}
-		
+	
 	}
 	
 	/*
 	 * @param   item id (int)
 	 * @return  fixed Discount on item
 	 */
-	public double getFixedDiscount(long itemID) {
-		
-		return ((this.fixedDiscount.containsKey(itemID)) ? this.fixedDiscount.get(itemID) : 0.0);
-		
-// 		if(this.fixedDiscount.containsKey(itemID)) {
-// 			return this.fixedDiscount.get(itemID);
-// 		}
-// 		else {
-// 			return noDiscount;
-// 		}
-		
+	public double getItemFixedDiscount(int itemID) {
+		Item item = this.items[this.searchItem(itemID)];
+		return item.getItemFixedDiscount();
 	}
 	
 	/*
@@ -92,9 +75,9 @@ public class FixedProductPromotion extends ShoppingCart implements Promotion {
 	 * @return  nothing
 	 * Set fixed discount on item 
 	 */
-	public void setFixedDiscount(long itemID, double discount) {
+	public void setItemFixedDiscount(int itemID, double discount) {
 		
-		this.fixedDiscount.put(itemID, discount);
+//		this.fixedDiscount.put(itemID, discount);
 		
 	}
 	
@@ -121,32 +104,5 @@ public class FixedProductPromotion extends ShoppingCart implements Promotion {
 			}
 		}
 		return false;
-	}
-	
-	public static void main(String args[]) {
-		FixedProductPromotion prodPromo = new FixedProductPromotion();
-		prodPromo.add(1, "SkyBag", 1156.36, 1);
-		prodPromo.add(2, "Algo Book", 560.36, 1);
-		prodPromo.add(3, "Laptop", 32156.36, 1);
-		prodPromo.add(4, "Samsung S8", 55556.36, 1);
-		prodPromo.add(5, "iPhone", 105556.36, 1);
-		
-		prodPromo.itemList();
-		
-		prodPromo.setPrice(1, 1000.0);
-		
-		prodPromo.itemList();
-		
-		prodPromo.setFixedDiscount(1, 10.0);
-		prodPromo.setFixedDiscount(2, 55.0);
-		prodPromo.setFixedDiscount(3, 30.0);
-		prodPromo.setFixedDiscount(4, 20.0);
-		prodPromo.setFixedDiscount(5, 15.0);
-	
-		System.out.println(prodPromo.isPromotionApplicable("sky10"));
-		
-		System.out.println(prodPromo.getFixedDiscount(5));
-		
-		System.out.println(prodPromo.getMinimumPrice(1, "sky10"));
 	}
 }
--- PF/PF-Assignment2/ShoppingCart.java
+++ PF/PF-Assignment2/ShoppingCart.java
@@ -1,27 +1,55 @@
-/*
- * Code by __E18/1025__
- * Shopping Cart for item 
- */
 
-public class ShoppingCart {
-	private Item firstItem ;
-	private Item lastItem;
-	private double total;
+public class ShoppingCart extends Item{
+	
+	private ItemList firstItem;
+	private ItemList lastItem;
+	private double totalAmountPayable;
+	Item items[];
+	private static int currentItemIndex;
 	
 	ShoppingCart(){
-		this.firstItem = null;
-		this.lastItem = null;
-		this.total = 0.0;
+		totalAmountPayable = 0.0;
+		ItemList firstItem = null;
+		ItemList lastItem = null;
+		items = new Item[100];
+		currentItemIndex = -1;
+	}
+	
+	static class ItemList{
+		
+		private int itemID;
+		private String itemName;
+		private double itemPrice;
+		private int itemQuantity;
+		private double itemTotalPrice;
+		private ItemList next;
+		
+		ItemList(int itemID, String itemName, double itemPrice, int itemQuantity){
+			this.itemID = itemID;
+			this.itemName = itemName;
+			this.itemPrice = itemPrice;
+			this.itemQuantity = itemQuantity;
+			this.itemTotalPrice = itemPrice * (double)itemQuantity;
+			this.next = null;
+		}
+		
+		public void setItemTotalPrice(double price) {
+			this.itemTotalPrice = price;
+		}
+		
+		public double getItemTotalPrice() {
+			return this.itemTotalPrice;
+		}
 	}
 	
 	/*
 	 * Enum for Promotion code 
 	 */
 	enum PromotionEnum{
-		SkyBag("sky10", "15/07/2018", "20/07/2018"),
-		Algo_Book("algo10", "15/07/2018", "20/07/2018"),
-		Laptop("laptop10", "15/07/2018", "20/07/2018"),
-		Samsung("samsung10", "15/07/2018", "20/07/2018");
+		SkyBag("sky10", "15/07/2018", "30/07/2018"),
+		Algo_Book("algo10", "15/07/2018", "30/07/2018"),
+		Laptop("laptop10", "15/07/2018", "30/07/2018"),
+		Samsung("samsung10", "15/07/2018", "30/07/2018");
 		
 		private String code, startDate, endDate;
 		
@@ -56,170 +84,122 @@ public class ShoppingCart {
 		}
 	}
 	
-	static class Item{
-		private long itemID;
-		private String itemName;
-		private double price;
-		private long quantity;
-		private Item next;
+	/*
+	 * Create Items for Shopping 
+	 * @param  itemID - item id
+	 * @param  itemName - Name of Item
+	 * @param  quantityInStock - Quantity in stock
+	 * @param  price - price of a item
+	 * @param  fixedDiscount - Fixed Discount on item
+	 * @return nothing
+	 */
+	public void createItem(int itemID, String itemName, int quantityInStock, double price, double fixedDiscount) {
+		this.items[ ++this.currentItemIndex ] = new Item(itemID, itemName, quantityInStock, price, fixedDiscount);
+	} 
+	
+	/*
+	 * Search an item and return index of item otherwise return -1
+	 * @param   itemID - Id of item
+	 * @return  int - Index of item if it is present otherwise -1
+	 */
+	public int searchItem(int itemID) {
 		
-		Item(long itemID, String itemName, double price, long quantity){
-			this.itemID = itemID;
-			this.itemName = itemName;
-			this.price = price;
-			this.quantity = quantity;
-			this.next = null;
+		for(int index=0; index<this.items.length; index++) {
+			if(this.items[index].getItemID() == itemID) {
+				return index;
+			}
 		}
+		return -1;
 	}
 	
 	/*
-	 * @param   item id (int)
-	 * @return  item price
+	 * Search Item in Shopping cart line
+	 * @param  itemID - item id
+	 * @return ItemList - item reference
 	 */
-	public double getPrice(long itemID) {
-		Item item;
-		item = this.searchItem(itemID);
-		return item.price;
-	}
-	
-	/*
-	 * @param   item id (int), price of item (double)
-	 * @return  nothing
-	 * Update Item price after discount
-	 */
-	public void setPrice(long itemID, double price) {
-		Item item;
-		item = this.searchItem(itemID);
-		item.price = price;
-	}
-	
-	/*
-	 * @param   nothing
-	 * @return  true if Shopping Cart is Empty otherwise false
-	 */
-	public boolean isEmpty() {
-		if (this.firstItem == null)
-			return true;
-		return false;
-	}
-	
-	/*
-	 * @param   item name (string), item price (double), item quantity(long)
-	 * @return  nothing
-	 * Add item into the Shopping Cart list
-	 */
-	public void add(long itemID, String itemName, double price, long quantity) {
-		Item newItem = new Item(itemID, itemName, price, quantity);
-		total += price * (double)quantity;
-		if(isEmpty()) {
-			firstItem = newItem;
-			lastItem = newItem;
-		}
-		else {
-			lastItem.next = newItem;
-			lastItem = lastItem.next;
-		}
-	}
-	
-	/*
-	 * @param   Item id 
-	 * @return  nothing
-	 * Remove Item from list of Shopping cart
-	 */
-	public void remove(long itemID) {
-		Item it = firstItem;
-		Item pre = firstItem;
-		while(it != null) {
-			if(it.itemID == itemID)
-				break;
-			pre = it;
-			it = it.next;
-		}
-		if(it == firstItem) 
-			firstItem = firstItem.next;
-		else if(it == lastItem) {
-			lastItem = pre;
-			pre.next = null;
-		}
-		else 
-			pre.next = it.next;
-		System.out.println("Item id-" + it.itemID + " Removed !");
-		it = null;
-	}
-	
-	/*
-	 * @param   item-id (long)
-	 * @return  Item reference if it is found otherwise null
-	 */
-	public Item searchItem(long item_id) {
-		Item it = firstItem;
-		while(it != null) {
-			if(it.itemID == item_id)
-				return it;
-			it = it.next;
+	public ItemList searchItemInCart(int itemID) {
+		ItemList itemList = this.firstItem;
+		while(itemList != null) {
+			if(itemList.itemID == itemID) {
+				return itemList;
+			}
+			itemList = itemList.next;
 		}
 		return null;
 	}
 	
 	/*
-	 * @param   item-id(long), change ("+" or "-")(String), Quantity(long)
-	 * @return  nothing
-	 * + used for Increase Quantity
-	 * - used for Decrease Quantity
+	 * Print List of items in stock
+	 * @param  nothing
+	 * @return nothing
 	 */
-	public void changeQuantity(long itemID, String change, long quantity) {
-		Item item = this.searchItem(itemID);
-		if (item == null)
-			System.out.println("Item is not found !");
+	public void ListOfItems() {
+		System.out.println("\nItem Prsent in Stock--------------------------------------------------------------");
+		System.out.println("Item ID --- Item Name --- Quantity of Item --- Item price --- fixed Discount item");
+		for(int index=0; index<=this.currentItemIndex; index++) {
+			System.out.print(this.items[index].getItemID() + " --- " + this.items[index].getItemName() + " --- ");
+			System.out.print(this.items[index].getItemQuantityInStock() + " --- ");
+			System.out.println(this.items[index].getItemPrice() + " --- " + this.items[index].getItemFixedDiscount());
+		}
+		System.out.println("-----------------------------------------------------------------------------------\n");
+	}
+	
+	/*
+	 * Choose item for shopping in stock
+	 * @param  itemID - item id 
+	 * @param  quantity - Quantity of item
+	 * @return nothing
+	 */
+	public void chooseItemForShopping(int itemID, int quantity) {
+		
+		Item item = this.items[this.searchItem(itemID)];
+		if(this.firstItem == null) {
+			this.firstItem = new ItemList(item.getItemID(), item.getItemName(), item.getItemPrice(), quantity);
+			this.lastItem = this.firstItem;
+			this.totalAmountPayable += firstItem.itemTotalPrice;
+		}
 		else {
-			long newQuantity = (change.equals("+") ? quantity : quantity*(-1)) ;
-			long preQuantity = item.quantity;
-			item.quantity += newQuantity;
-			if(item.quantity < 0) {
-				newQuantity = preQuantity*(-1);
-				item.quantity = 0;
-			}
-			total += item.price * (double)newQuantity;
-			System.out.println("Item id-" + itemID + " Quantity is Changed !");
-			if(item.quantity == 0)
-				this.remove(item.itemID);
+			this.lastItem.next = new ItemList(item.getItemID(), item.getItemName(), item.getItemPrice(), quantity);
+			this.lastItem = this.lastItem.next;
+			this.totalAmountPayable += this.lastItem.itemTotalPrice;
 		}
 	}
 	
-	
 	/*
-	 * @param   nothing
-	 * @return  nothing
-	 * Print list of all items present in Shopping Cart
+	 * Print Item Present in Shopping Cart
+	 * @param  nothing
+	 * @return nothing
 	 */
-	public void itemList() {
-		Item it = firstItem;
-		while(it != null) {
-			System.out.println(it.itemID + "---" + it.itemName + "---" + it.price + "---" + it.quantity );
-			it = it.next;
+	public void itemInCart() {
+		System.out.println("\nItem Prsent in Cart----------------------------------------------------");
+		System.out.println("Item ID --- Item Name --- Item Price --- Item Quantity --- total Price");
+		ItemList itemList = this.firstItem;
+		while(itemList != null) {
+			System.out.print(itemList.itemID + " --- " + itemList.itemName + " --- ");
+			System.out.print(itemList.itemPrice + " --- " + itemList.itemQuantity + " --- ");
+			System.out.println(itemList.itemTotalPrice);
+			itemList = itemList.next;
 		}
-	}
-	
-	public double totalAmountPayable() {
-		return this.total;
+		System.out.println("\n          Total Amount Payalbe : " + this.totalAmountPayable);
+		System.out.println("-----------------------------------------------------------------------\n");
 	}
 	
 	/*
-	public static void main(String args[]) {
-		ShoppingCart shopCart = new ShoppingCart();
-		shopCart.add(1, "SkyBag", 1156.36, 1);
-		shopCart.add(2, "Algo Book", 560.36, 1);
-		shopCart.add(3, "Laptop", 32156.36, 1);
-		shopCart.add(4, "Samsung S8", 55556.36, 1);
-		shopCart.add(5, "iPhone", 105556.36, 1);
-		
-		shopCart.itemList();
-		System.out.println(shopCart.totalAmountPayable());
-		
-		shopCart.changeQuantity(2, "-", 2);
-		
-		shopCart.itemList();
-		System.out.println(shopCart.totalAmountPayable());
+	 * Update Quantity of Item in Shopping Cart
+	 * @param  nothing
+	 * @return nothing
+	 */
+	public void updateQuantity(int itemID, int quantity) {
+		ItemList item = this.firstItem;
+		while(item != null) {
+			if(item.itemID == itemID) {
+				this.totalAmountPayable -= item.itemTotalPrice;
+				item.itemQuantity = quantity;
+				item.itemTotalPrice = quantity * (double)item.itemPrice;
+				this.totalAmountPayable += item.itemTotalPrice;
+				System.out.println("\n--------Item "+ item.itemName+ " quantity changed - " + quantity + "--------\n");
+			}
+			item = item.next;
+		}
 	}
-	*/
 }
