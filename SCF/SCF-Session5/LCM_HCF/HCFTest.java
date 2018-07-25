b250189819ee98304fa805321d1a341a1ba149fe
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-25 10:10:45 +0530

    HCFTest.java  updated !

--- SCF/SCF-Session5/LCM_HCF/HCFTest.java
+++ SCF/SCF-Session5/LCM_HCF/HCFTest.java
@@ -6,24 +6,24 @@ import org.junit.jupiter.api.Test;
 class HCFTest {
 
 	@Test
-	void test1() {
+	void testHCFCalculatedCorrectly1() {
 		
 		HCF hcf = new HCF();
-		Assertions.assertEquals(5, hcf.hcfOf(15, 35), "Wrong Answer");
+		Assertions.assertEquals(5, hcf.hcfOf(15, 35), "Wrong Answer ! HCF of 15 and 35 will be 5");
 	}
 	
 	@Test
-	void test2() {
+	void testHCFCalculatedCorrectly2() {
 		
 		HCF hcf = new HCF();
-		Assertions.assertEquals(7, hcf.hcfOf(35, 21), "Wrong Answer");
+		Assertions.assertEquals(7, hcf.hcfOf(35, 21), "Wrong Answer ! HCF of 35 and 21 will be 7");
 	}
 	
 	@Test
-	void test3() {
+	void testHCFCalculatedCorrectly3() {
 		
 		HCF hcf = new HCF();
-		Assertions.assertEquals(5, hcf.hcfOf(30, 24), "Wrong Answer");
+		Assertions.assertEquals(6, hcf.hcfOf(30, 24), "Wrong Answer ! HCF of 30 and 24 will be 6");
 	}
 
 }
