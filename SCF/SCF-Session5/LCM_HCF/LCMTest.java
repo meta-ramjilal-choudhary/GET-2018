716a0cd08e1e92b201f5eaaf872132f983048f4b
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-25 10:15:25 +0530

    LCMTest.java Updated !

--- SCF/SCF-Session5/LCM_HCF/LCMTest.java
+++ SCF/SCF-Session5/LCM_HCF/LCMTest.java
@@ -6,24 +6,24 @@ import org.junit.jupiter.api.Test;
 class LCMTest {
 
 	@Test
-	void test1() {
+	void testLCMCalculatedCorrectly1() {
 		LCM lcm = new LCM();
 	
-		Assertions.assertEquals(24, lcm.lcmOf(8, 12), "Wrong Answer");
+		Assertions.assertEquals(24, lcm.lcmOf(8, 12), "Wrong Answer! LCM of 8 and 12 will be 24");
 	}
 	
 	@Test
-	void test2() {
+	void testLCMCalculatedCorrectly2() {
 		LCM lcm = new LCM();
 	
-		Assertions.assertEquals(75, lcm.lcmOf(25, 15), "Wrong Answer");
+		Assertions.assertEquals(25, lcm.lcmOf(25, 25), "Wrong Answer! LCM of 25 and 25 will be 25");
 	}
 
 	@Test
-	void test3() {
+	void testLCMCalculatedCorrectly3() {
 		LCM lcm = new LCM();
 	
-		Assertions.assertEquals(20, lcm.lcmOf(8, 6), "Wrong Answer");
+		Assertions.assertEquals(195, lcm.lcmOf(65, 15), "Wrong Answer! LCM of 65 and 15 will be 195");
 	}
 }
 
