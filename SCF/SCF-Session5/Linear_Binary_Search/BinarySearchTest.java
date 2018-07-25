21656dbaa6252a0566d1ec53fcfac8d69f07793c
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-25 10:31:03 +0530

    BinarySearchTest.java  updated!

--- SCF/SCF-Session5/Linear_Binary_Search/BinarySearchTest.java
+++ SCF/SCF-Session5/Linear_Binary_Search/BinarySearchTest.java
@@ -6,27 +6,27 @@ import org.junit.jupiter.api.Test;
 class BinarySearchTest {
 
 	@Test
-	void test1() {
+	void testBinarySearch1() {
 		BinarySearch bs = new BinarySearch();
 		int array[] = {1, 2, 3, 45, 200, 2344};
 		
-		Assertions.assertEquals(4, bs.search(array, 0, array.length, 200), "Wrong Answer");
+		Assertions.assertEquals(4, bs.search(array, 200), "Wrong Answer! Element is not found");
 	}
 
 	@Test
-	void test2() {
+	void testBinarySearch2() {
 		BinarySearch bs = new BinarySearch();
 		int array[] = {1, 2, 3, 45, 200, 2344};
 		
-		Assertions.assertEquals(0, bs.search(array, 0, array.length, 1), "Wrong Answer");
+		Assertions.assertEquals(0, bs.search(array, 1), "Wrong Answer! Element is not found");
 	}
 
 	@Test
-	void test3() {
+	void testBinarySearch3() {
 		BinarySearch bs = new BinarySearch();
 		int array[] = {1, 2, 3, 45, 200, 2344};
 		
-		Assertions.assertEquals(-1, bs.search(array, 0, array.length, 2000), "Wrong Answer");
+		Assertions.assertEquals(-1, bs.search(array, 2000), "Wrong Answer! Element is found");
 	}
 
 }
