d5b36e267ec7bfb20338d0216b9f98a89f732ccc
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-25 10:36:37 +0530

    LinearSearchTest.java Updated!

--- SCF/SCF-Session5/Linear_Binary_Search/LinearSearchTest.java
+++ SCF/SCF-Session5/Linear_Binary_Search/LinearSearchTest.java
@@ -6,27 +6,19 @@ import org.junit.jupiter.api.Test;
 class LinearSearchTest {
 
 	@Test
-	void test1() {
+	void testLinearSearch1() {
 		
 		int array[] = {1, 2, 3, 53, 23, 0, -23, 56, -12};
 		LinearSearch ls = new LinearSearch();
-		Assertions.assertEquals(3, ls.search(array, 0, 53), "Wrong Answer");
+		Assertions.assertEquals(3, ls.search(array, 53), "Wrong Answer! Element is not found");
 	}
 	
 	@Test
-	void test2() {
+	void testLinearSearch2() {
 		
 		int array[] = {1, 2, 3, 56, 23, 0, -23, 56, -12};
 		LinearSearch ls = new LinearSearch();
-		Assertions.assertEquals(-1, ls.search(array, 0, 333), "Wrong Answer");
-	}
-	
-	@Test
-	void test3() {
-		
-		int array[] = {1, 2, 3, 56, 23, 0, -23, 56, -12};
-		LinearSearch ls = new LinearSearch();
-		Assertions.assertEquals(8, ls.search(array, 0, -12), "Wrong Answer");
+		Assertions.assertEquals(-1, ls.search(array, 333), "Wrong Answer! Element is found");
 	}
 
 }
