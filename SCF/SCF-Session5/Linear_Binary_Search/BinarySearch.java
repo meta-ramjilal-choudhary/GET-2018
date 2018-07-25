00055cf7fa129bb79f3ed51e90e51582d34009ec
meta-ramjilal-choudhary <41181691+meta-ramjilal-choudhary@users.noreply.github.com>
2018-07-25 10:29:44 +0530

    BinarySearch.java  Updated !

--- SCF/SCF-Session5/Linear_Binary_Search/BinarySearch.java
+++ SCF/SCF-Session5/Linear_Binary_Search/BinarySearch.java
@@ -6,23 +6,33 @@ public class BinarySearch {
 	BinarySearch(){}
 	
 	/*
+	 * Find Element in array using Binary Search
+	 * @param  array - Array of Inegers
+	 * @param  value - integer value
+	 * @return int - index of value if it is found otherwise -1
+	 */
+	public int search(int array[], int value) {
+		return this.searchValue(array, 0, array.length-1, value);
+	}
+	
+	/*
 	 * Find element in an Array using Binary search
 	 * @param  array - Array of integers
 	 * @param  startIndex - Starting index 
 	 * @param  value - value to be found in array
 	 * @return int - index of value if it is find otherwise -1
 	 */
-	public int search(int array[], int startIndex, int endIndex, int value) throws ArrayIndexOutOfBoundsException,ArithmeticException {
+	private int searchValue(int array[], int startIndex, int endIndex, int value) throws ArrayIndexOutOfBoundsException,ArithmeticException {
 		if(startIndex > endIndex) {
 			return -1;
 		}
 		
 		int mid = (startIndex + endIndex)/2;
 		if(value < array[mid]) {
-			return search(array, startIndex, mid-1, value);
+			return searchValue(array, startIndex, mid-1, value);
 		}
 		else if(value > array[mid]) {
-			return search(array, mid+1, endIndex, value);
+			return searchValue(array, mid+1, endIndex, value);
 		}
 		return mid;
 	}
