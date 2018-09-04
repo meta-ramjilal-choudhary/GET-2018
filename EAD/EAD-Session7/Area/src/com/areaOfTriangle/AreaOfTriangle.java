19fd72e7307fc6afafaa471dc1d2586cdde36aea
Ramjilal choudhary <ramjilal.choudhary@metacube.com>
2018-09-04 13:14:19 +0530

    EAD/EAD-Session7-assignment

---
+++ EAD/EAD-Session7/Area/src/com/areaOfTriangle/AreaOfTriangle.java
@@ -0,0 +1,14 @@
+package com.areaOfTriangle;
+
+public class AreaOfTriangle {
+
+	/**
+	 * Return area of triangle
+	 * @param height - height of triangle
+	 * @param base - base of triangle
+	 * @return - area of triangle
+	 */
+	public double getArea(double height, double base){
+		return height * base * 0.5;
+	}
+}
